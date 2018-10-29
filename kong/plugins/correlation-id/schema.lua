0cb86a8a8db4d09ed6e2c178eda193970062bdbc
Enrique García Cota <kikito@gmail.com>
2018-10-29 17:01:41 +0100

    feat(correlation-id) tag correlation-id as mesh-two-way
    Correlation-id will be mostly used on inbound traffic, but it could be used in outbound traffic too - notice that it can optionally set a header in the response as well as in the service request.

--- kong/plugins/correlation-id/schema.lua
+++ kong/plugins/correlation-id/schema.lua
@@ -1,9 +1,13 @@
+local typedefs = require "kong.db.schema.typedefs"
+
+
 return {
   name = "correlation-id",
   fields = {
     { config = {
         type = "record",
         fields = {
+          { mesh_mode = typedefs.mesh_mode_two_way },
           { header_name = { type = "string", default = "Kong-Request-ID" }, },
           { generator = { type = "string", default = "uuid#counter",
                           one_of = { "uuid", "uuid#counter", "tracker" }, }, },
